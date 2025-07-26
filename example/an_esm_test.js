2badcf370e9bc30550970758b2caf64a0d50800c
eeue56 <enalicho@gmail.com>
2025-07-26 18:02:33 +0200

    add some more example files

---
+++ example/an_esm_test.js
@@ -0,0 +1,5 @@
+export function helloWorld() {
+  console.log("hi world!");
+
+  document.body.appendChild(document.createTextNode("Sure?"));
+}
--- example/main.js
+++ example/main.js
@@ -1,5 +1,9 @@
+import { helloWorld } from "./an_esm_test.js";
+
 function main() {
-  document.body.appendChild(document.createTextNode("Hi there"));
+  document.body.appendChild(document.createTextNode("Hi there! "));
+
+  helloWorld();
 }
 
 main();
